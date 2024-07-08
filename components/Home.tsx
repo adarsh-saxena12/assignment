bee4db6259c1ec9922bed2abe74c9532c7078d8e
Adarsh Saxena <saxenaadarsh98@gmail.com>
2024-07-08 14:59:16 +0530

    added button

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
 import FormPage from './FormPage';
+import Button from '@mui/material/Button';
 
 const Home = () => {
- 
     return (
         <div>
+            <Link to="/success" style={{ textDecoration: 'none' }}>
+                <Button variant="contained" color="primary">
+                    Next Page
+                </Button>
+            </Link>
             <FormPage />
         </div>
     );
